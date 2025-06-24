ae27d3e17b7c21b742631dc8c1c60880fe9ea97a
stevep <stevep_pan@yahoo.com>
2025-06-24 16:41:44 +0300

    feat: add insert or update logic

---
+++ src/gr/aueb/cf/bankapp/dao/AccountDAOImpl.java
@@ -0,0 +1,36 @@
+package gr.aueb.cf.bankapp.dao;
+
+import gr.aueb.cf.bankapp.model.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class AccountDAOImpl implements IAccountDAO {
+    private final List<Account> accounts = new ArrayList<>();
+    @Override
+    public void saveOrUpdate(Account account) {
+        int index = accounts.indexOf(account);
+
+        if(index != -1){
+            accounts.set(index, account);
+            return;
+        }
+        accounts.add(account);
+    }
+
+    @Override
+    public void remove(String iban) {
+
+    }
+
+    @Override
+    public Optional<Account> getByIban(String iban) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Account> getAccounts() {
+        return List.of();
+    }
+}
